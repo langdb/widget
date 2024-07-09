69c80a2e3a1f33b6bac52c4e9b6473be6b4196e1
duonganhthu43 <duonganhthu43@gmail.com>
2024-07-09 14:13:03 +0700

    chore: update favicon and icon

---
+++ .storybook/manager.ts
@@ -0,0 +1,6 @@
+import { addons } from '@storybook/manager-api';
+import langdbTheme from './langdb';
+
+addons.setConfig({
+  theme: langdbTheme,
+});
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,6 +1,8 @@
 import type { Preview } from "@storybook/react";
+
 const preview: Preview = {
   parameters: {
+    layout: 'centered',
     controls: {
       matchers: {
         color: /(background|color)$/i,
