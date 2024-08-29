4b22cd47a7143bf6f271f1bdc07e89f181099bf1
duonganhthu43 <duonganhthu43@gmail.com>
2024-08-29 13:56:37 +0700

    fix: copy btn

--- src/components/Messages/CopyToClipboard.tsx
+++ src/components/Messages/CopyToClipboard.tsx
@@ -1,15 +1,15 @@
 import { CheckIcon, ClipboardDocumentIcon } from "@heroicons/react/24/outline";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 export const CopyToClipboard: React.FC<{ content: string } & React.HTMLAttributes<HTMLDivElement>> = ({ content, className, ...restProps }) => {
   const [copied, setCopied] = useState(false);
 
-  const handleCopy = (event: React.MouseEvent<HTMLDivElement>) => {
+  const handleCopy = useCallback( (event: React.MouseEvent<HTMLDivElement>) => {
     event.preventDefault();
     navigator.clipboard.writeText(content);
     setCopied(true);
     setTimeout(() => setCopied(false), 3000);
-  };
+  }, [content]);
 
   return (
     <div className={`items-center flex flex-row text-xs ${className}`} onClick={handleCopy} {...restProps}>
--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -27,12 +27,12 @@ export const Widget: React.FC<WidgetProps> = React.memo((props) => {
   return (
     <div className={`flex flex-col w-[100%] h-full ${themeClass}`}>
       <div className="flex-1 w-full relative">
-        <main
+        <div
           className={`items-center justify-between ${className} h-full absolute`}
           style={props.style || {}}
         >
           <ChatComponent {...props} />
-        </main>
+        </div>
       </div>
     </div>
   );
